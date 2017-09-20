4a7bd2d9e370a9116fde3b2cd452bed7ff98c979
kumar achal <infosec.achal@gmail.com>
2017-09-20 15:54:18 +0530

    1st

---
+++ pyt/temp.py
@@ -0,0 +1,26 @@
+ 
+
+fam = [1.73, 1.68, 1.71, 1.89]
+print(fam)
+print(fam + fam)
+print(fam*3)
+fam= ["liz", 1.73, "emma", 1.68, "mom", 1.71, "dad", 1.89 ]
+print(fam)
+type(fam)
+list(fam)
+fam[3]
+fam[10]
+fam[-1]
+fam[3:5]
+fam[1:4]
+fam
+
+fam[0.2]= ["lisa", 1.74]
+fam + ["me", 1.79]
+fam.ext = fam +["me", 1.79]
+x = ["a","b","c"]
+y=x
+y[1]="z"
+y
+x
+
